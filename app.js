a86afb1bfcbec00af9b6ca9b74e3cb80464b0ab9
nymka <nymkanyambayar99@gmail.com>
2022-02-12 14:56:13 +0900

    shoogoo shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ var scores = [0, 0];
 //Toglogchiin eeljind tsugluulj onoo hadgalah
 var roudScore = 0;
 // shoonii onoo
-var dice = Math.floor(Math.random() * 6 + 1);
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-document.querySelector(".btn-roll").style.display = "none";
 
-console.log(" shoo " + dice);
+window.document.getElementById("score-0").textContent = "0";
+window.document.getElementById("score-0").textContent = "0";
+window.document.getElementById("current-0").textContent = "0";
+window.document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6 + 1);
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
